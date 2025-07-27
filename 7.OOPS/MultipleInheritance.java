82c7c42721986842785d0ba60e4f4dfd9043ee5e
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2025-07-27 21:21:13 +0530

    multiple inheritance

---
+++ 7.OOPS/MultipleInheritance.java
@@ -0,0 +1,13 @@
+interface Dog6 {
+    void sound();
+}
+
+interface Movable {
+    void movable();
+}
+
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        
+    }
+}
